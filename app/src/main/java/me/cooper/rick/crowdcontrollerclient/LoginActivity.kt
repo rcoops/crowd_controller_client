0ae024026fa80ef4e596e1bc58dc5e582436d8b7
rcoops <rcoops84@hotmail.com>
2018-02-01 12:15:30 +0000

    crappy login done

--- app/src/main/java/me/cooper/rick/crowdcontrollerclient/LoginActivity.kt
+++ app/src/main/java/me/cooper/rick/crowdcontrollerclient/LoginActivity.kt
@@ -1,30 +1,37 @@
 package me.cooper.rick.crowdcontrollerclient
 
+import android.Manifest.permission.READ_CONTACTS
 import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
 import android.annotation.TargetApi
-import android.content.pm.PackageManager
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity
 import android.app.LoaderManager.LoaderCallbacks
 import android.content.CursorLoader
+import android.content.Intent
 import android.content.Loader
+import android.content.pm.PackageManager
 import android.database.Cursor
 import android.net.Uri
 import android.os.AsyncTask
 import android.os.Build
 import android.os.Bundle
 import android.provider.ContactsContract
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
 import android.text.TextUtils
+import android.util.Log
 import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.widget.ArrayAdapter
 import android.widget.TextView
-
-import java.util.ArrayList
-import android.Manifest.permission.READ_CONTACTS
-
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_login.*
+import me.cooper.rick.crowdcontrollerapi.dto.Message
+import org.springframework.http.*
+import org.springframework.http.converter.json.MappingJacksonHttpMessageConverter
+import org.springframework.web.client.HttpClientErrorException
+import org.springframework.web.client.ResourceAccessException
+import org.springframework.web.client.RestTemplate
+import java.util.*
 
 /**
  * A login screen that offers login via email/password.
@@ -34,6 +41,10 @@ class LoginActivity : AppCompatActivity(), LoaderCallbacks<Cursor> {
      * Keep track of the login task to ensure we can cancel it if requested.
      */
     private var mAuthTask: UserLoginTask? = null
+
+    private val REQUEST_READ_CONTACTS = 1
+
+    protected val TAG = LoginActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -104,7 +115,7 @@ class LoginActivity : AppCompatActivity(), LoaderCallbacks<Cursor> {
         password.error = null
 
         // Store values at the time of the login attempt.
-        val emailStr = username.text.toString()
+        val usernameStr = username.text.toString()
         val passwordStr = password.text.toString()
 
         var cancel = false
@@ -118,7 +129,7 @@ class LoginActivity : AppCompatActivity(), LoaderCallbacks<Cursor> {
         }
 
         // Check for a valid email address.
-        if (TextUtils.isEmpty(emailStr)) {
+        if (TextUtils.isEmpty(usernameStr)) {
             username.error = getString(R.string.error_field_required)
             focusView = username
             cancel = true
@@ -137,7 +148,7 @@ class LoginActivity : AppCompatActivity(), LoaderCallbacks<Cursor> {
             // Show a progress spinner, and kick off a background task to
             // perform the user login attempt.
             showProgress(true)
-            mAuthTask = UserLoginTask(emailStr, passwordStr)
+            mAuthTask = UserLoginTask(usernameStr, passwordStr)
             mAuthTask!!.execute(null as Void?)
         }
     }
@@ -149,7 +160,7 @@ class LoginActivity : AppCompatActivity(), LoaderCallbacks<Cursor> {
 
     private fun isPasswordValid(password: String): Boolean {
         //TODO: Replace this with your own logic
-        return password.length > 4
+        return true//password.length > 4
     }
 
     /**
@@ -228,6 +239,12 @@ class LoginActivity : AppCompatActivity(), LoaderCallbacks<Cursor> {
         username.setAdapter(adapter)
     }
 
+    private fun displayResponse(response: Message) {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.putExtra("message", response.text)
+        startActivity(intent)
+    }
+
     object ProfileQuery {
         val PROJECTION = arrayOf(
                 ContactsContract.CommonDataKinds.Email.ADDRESS,
@@ -240,57 +257,43 @@ class LoginActivity : AppCompatActivity(), LoaderCallbacks<Cursor> {
      * Represents an asynchronous login/registration task used to authenticate
      * the user.
      */
-    inner class UserLoginTask internal constructor(private val mEmail: String, private val mPassword: String) : AsyncTask<Void, Void, Boolean>() {
+    inner class UserLoginTask internal constructor(private val username: String, private val password: String) : AsyncTask<Void, Void, Message>() {
 
-        override fun doInBackground(vararg params: Void): Boolean? {
-            // TODO: attempt authentication against a network service.
+//        override fun onPreExecute() {
+//            showLoadingProgressDialog()
+//        }
 
-            try {
-                // Simulate network access.
-                Thread.sleep(2000)
-            } catch (e: InterruptedException) {
-                return false
+        override fun doInBackground(vararg params: Void): Message {
+            val url = getString(R.string.base_uri) + "/getmessage"
+
+            // Populate the HTTP Basic Authentication header with the username and password
+            val authHeader = HttpBasicAuthentication(username, password)
+            val requestHeaders = HttpHeaders()
+            requestHeaders.setAuthorization(authHeader)
+            requestHeaders.accept = listOf(MediaType.APPLICATION_JSON)
+
+            // Create a new RestTemplate instance
+            val restTemplate = RestTemplate()
+            restTemplate.messageConverters.add(MappingJacksonHttpMessageConverter())
+
+            return try {
+                // Make the network request
+                Log.d(TAG, url)
+                val response = restTemplate.exchange(url, HttpMethod.GET, HttpEntity<Any>(requestHeaders), Message::class.java)
+                response.body
+            } catch (e: HttpClientErrorException) {
+                Log.e(TAG, e.localizedMessage, e)
+                Message(0, e.statusText, e.localizedMessage)
+            } catch (e: ResourceAccessException) {
+                Log.e(TAG, e.localizedMessage, e)
+                Message(0, e.javaClass.simpleName, e.localizedMessage)
             }
 
-            return DUMMY_CREDENTIALS
-                    .map { it.split(":") }
-                    .firstOrNull { it[0] == mEmail }
-                    ?.let {
-                        // Account exists, return true if the password matches.
-                        it[1] == mPassword
-                    }
-                    ?: true
         }
 
-        override fun onPostExecute(success: Boolean?) {
-            mAuthTask = null
-            showProgress(false)
-
-            if (success!!) {
-                finish()
-            } else {
-                password.error = getString(R.string.error_incorrect_password)
-                password.requestFocus()
-            }
+        override fun onPostExecute(result: Message) {
+            displayResponse(result)
         }
 
-        override fun onCancelled() {
-            mAuthTask = null
-            showProgress(false)
-        }
-    }
-
-    companion object {
-
-        /**
-         * Id to identity READ_CONTACTS permission request.
-         */
-        private val REQUEST_READ_CONTACTS = 0
-
-        /**
-         * A dummy authentication store containing known user names and passwords.
-         * TODO: remove after connecting to a real authentication system.
-         */
-        private val DUMMY_CREDENTIALS = arrayOf("foo@example.com:hello", "bar@example.com:world")
     }
 }
